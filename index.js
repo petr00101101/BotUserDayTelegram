72c0d282c25bbe271caa797e3c8e9da11cb936b9
Nikita Striuk <nikita.striuk@datarobot.com>
2020-04-04 12:51:03 +0300

    eslint, prettier, husky/lint-staged, editorconfig, gitignore setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const Telegraf = require('telegraf');
+
+const bot = new Telegraf('1216019956:AAHeETzmnZ5aS-_aeu3CLa00BT1gXCZFNVY');
+
+// display Welcome text when we start bot
+bot.start((ctx) => ctx.reply('Welcome'));
+
+// listen and handle when user type hi text
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.launch();
